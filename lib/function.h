2f8c8e02f75d6e9807abeadd9b4a8c89eb9a95aa
BECK Maxime <maxime.beck@epitech.eu>
2023-03-14 21:37:48 +0100

    MOVE sprite.c in lib

--- lib/function.h
+++ lib/function.h
@@ -5,5 +5,12 @@
 #include <SFML/System.h>
 #include <SFML/Window.h>
 
+//! window.c
+
 sfRenderWindow *create_window(sfVideoMode mode, sfUint32 style);
 sfRenderWindow *window_reformat(sfRenderWindow *w, sfVideoMode v_mode, sfUint32 style);
+
+//! sprite.c
+
+void Sprite_move_absolute(sfSprite **sprite, sfVector2f new_pos);
+void Sprite_move_relative(sfSprite **sprite , sfVector2f new_pos);
