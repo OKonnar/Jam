7626c47d9e5dd5297bc166d9e95299734e5430a1
Djelou Daryl <daryl.djelou@epitech.fr>
2023-03-18 16:19:08 +0100

    feat(intro sprite animated)

--- src/menu_scene/init_scene.c
+++ src/menu_scene/init_scene.c
@@ -4,6 +4,7 @@
 void init_scene_menu(scene_manager_t *manager)
 {
     scene_t *scene = manager->create_scene(&manager);
+    sfSprite *intro = scene->add_sprite("intro", "./assets/sprites/intro.png", &scene, &(sfIntRect){0,0, 280,160}, NONE);
     sfSprite *play_button = scene->add_sprite("play_button", "./assets/sprites/press_to_start.png", &scene, &(sfIntRect){0, 0, 507, 54}, CLICK_AND_HOVER);
     sfSprite_setOrigin(play_button, (sfVector2f){507 / 2,  54 / 2});
     sfSprite_setScale(play_button, (sfVector2f){1, 1});
--- src/menu_scene/update_scene.c
+++ src/menu_scene/update_scene.c
@@ -3,5 +3,9 @@
 void update_scene_menu(scene_t *scene)
 {
     sprite_t *play_button = (sprite_t *)scene->find_object("play_button", scene->objects);
+    sprite_t *intro = (sprite_t *)scene->find_object("intro", scene->objects);
     update_rect(play_button->sprite, (int[1]){11}, 8);
+    if (sfKeyboard_isKeyPressed(sfKeySpace)) {
+        update_rect(intro->sprite,(int[1]){54}, 54);
+    }
 }
